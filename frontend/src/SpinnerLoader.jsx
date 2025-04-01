00eae9a866a15f961a8f4f773d8d9268b9f936a5
Aditya Jaiswal <134221262+adityajais8090@users.noreply.github.com>
2025-04-01 21:28:37 +0530

    Create SpinnerLoader.jsx  setup html tag

---
+++ frontend/src/SpinnerLoader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loader.css"; // Ensure this file exists
+
+const Loader = () => {
+  return (
+    <div className="spinner-loader">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Loader;
